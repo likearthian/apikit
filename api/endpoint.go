b42d298b03434e087f7bb273e6f538621a4dd56a
Ziska Zarkasyi <ziska.zarkasyi@gmail.com>
2023-03-01 10:02:58 +0700

    add new apikit

--- api/endpoint.go
+++ api/endpoint.go
@@ -2,47 +2,17 @@ package api
 
 import (
 	"context"
+
+	"github.com/go-kit/kit/endpoint"
 )
 
-// Endpoint is the fundamental building block of servers and clients.
-// It represents a single RPC method.
-type Endpoint[I any, O any] func(ctx context.Context, request I) (response O, err error)
+type ServiceFunc[I any, O any] func(ctx context.Context, req I)
 
-func (ep Endpoint[I, O]) Chain(middlewares ...Middleware[I, O]) Endpoint[I, O] {
-	for i := len(middlewares) - 1; i >= 0; i-- {
-		ep = middlewares[i](ep)
-	}
+func MakeEndpoint[I any, O any](svc func(context.Context, I) O) endpoint.Endpoint {
+	return func(ctx context.Context, request any) (any, error) {
+		req, ok := request.(I)
+		if !ok {
 
-	return ep
-}
-
-// Nop is an endpoint that does nothing and returns a nil error.
-// Useful for tests.
-func Nop(context.Context, interface{}) (interface{}, error) { return struct{}{}, nil }
-
-// Middleware is a chainable behavior modifier for endpoints.
-type Middleware[I any, O any] func(Endpoint[I, O]) Endpoint[I, O]
-
-// Chain is a helper function for composing middlewares. Requests will
-// traverse them in the order they're declared. That is, the first middleware
-// is treated as the outermost middleware.
-func Chain[I any, O any](outer Middleware[I, O], others ...Middleware[I, O]) Middleware[I, O] {
-	return func(next Endpoint[I, O]) Endpoint[I, O] {
-		for i := len(others) - 1; i >= 0; i-- { // reverse
-			next = others[i](next)
 		}
-		return outer(next)
 	}
-}
-
-// Failer may be implemented by Go kit response types that contain business
-// logic error details. If Failed returns a non-nil error, the Go kit transport
-// layer may interpret this as a business logic error, and may encode it
-// differently than a regular, successful response.
-//
-// It's not necessary for your response types to implement Failer, but it may
-// help for more sophisticated use cases. The addsvc example shows how Failer
-// should be used by a complete application.
-type Failer interface {
-	Failed() error
 }
