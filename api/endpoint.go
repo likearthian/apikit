e2053b4632f0a4f678a4d1ceb40ebc9721de94ae
Ziska Zarkasyi <ziska.zarkasyi@gmail.com>
2024-01-01 22:23:24 +0700

    Refactor middleware chaining in endpoint.go

--- api/endpoint.go
+++ api/endpoint.go
@@ -16,21 +16,6 @@ func (ep Endpoint[I, O]) Chain(outer Middleware[I, O], others ...Middleware[I, O
 
 func Nop(context.Context, interface{}) (interface{}, error) { return struct{}{}, nil }
 
-// Middleware is a chainable behavior modifier for endpoints.
-type Middleware[I, O any] func(Endpoint[I, O]) Endpoint[I, O]
-
-// Chain is a helper function for composing middlewares. Requests will
-// traverse them in the order they're declared. That is, the first middleware
-// is treated as the outermost middleware.
-func Chain[I, O any](outer Middleware[I, O], others ...Middleware[I, O]) Middleware[I, O] {
-	return func(next Endpoint[I, O]) Endpoint[I, O] {
-		for i := len(others) - 1; i >= 0; i-- { // reverse
-			next = others[i](next)
-		}
-		return outer(next)
-	}
-}
-
 // Failer may be implemented by Go kit response types that contain business
 // logic error details. If Failed returns a non-nil error, the Go kit transport
 // layer may interpret this as a business logic error, and may encode it
